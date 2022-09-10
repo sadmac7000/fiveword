efcfb1207371d0fcd03153f55a94798d72bf24c2
Casey Dahlin <sadmac@google.com>
2022-09-10 02:32:43 -0700

    New faster algorithm

--- README.md
+++ README.md
@@ -16,38 +16,23 @@ connected if they share no common letters. The algorithm then locates 5-cliques 
 discover solutions. Matt's annecdotal test of this program said it ran in about 15 minutes on the
 same data set.
 
-The solution here resembles Matt's solution more closely than Benjamin's. For each item in the list
-of words, we scan the remaining words to see if we can find a word with no letters in common. Then
-we scan the words *to the right of that word* to see if we can find a third word which has no letter
-in common with either of the first two. For each such word we find, we scan the words *to the right
-of that word* to find a fourth disjoint word, and so on for the fifth. This exploitation of the
-order of the list means each subsequent word added to a given solution can be scanned from a smaller
-list than the previous, reducing the time complexity of the search. This avoids inefficiency in
-the second step of Matt's algorithm, where two pairs might share an entire word in common but are
-still compared in the second pass. This should be a better optimization over the naive n^5 solution
-than Matt's pairing system.
+The solution here is a hybrid approach. We assemble a tree where each node contains in effect a set
+of letters corresponding to a single word. We insert each word into the tree as follows
+1) Insert a new node as the child of the root.
+2) For each previously existing child of the root, if it has no letters in common with the new word,
+   insert into the subtree rooted at that child.
+
+Once assembled, every path from the root to a leaf is a solution to our problem.
 
 The second optimization we do is in the comparison of words themselves. Matt reduces words to python
 `set`s of characters and tests commonality by merging the sets then checking the cardinality of the
 result. Instead, we map each word to a 32-bit unsigned integer, where each bit in the integer is
 non-zero if an only if the letter of the alphabet sharing that bit's index is present in the word.
 So bit 0 is set if the word contains the letter 'a', bit 1 if it contains the letter 'b', etc. At a
-basic level this is simply a highly machine-optimized representation of a set of characters, but we
-further enhance our algorithm by storing our list of words as a B-Tree mapping from integer keys
-describing the set of characters as shown to the words themselves. When we test a given word, we can
-use a few bitwise operations to calculate the smallest possible next value that might map to a word
-meeting our criteria, and use the B-Tree structure to skip ahead to that value.
+basic level this is simply a highly machine-optimized representation of a set of characters, meaning each node in the tree is only an 
 
-Finally, there are lower-level optimizations; both Matt and Benjamin's solutions are in Python, this
-solution is in Rust, and both Benjamin and (as far as we know) Matt's solutions are single-threaded.
-This solution is parallelized.
+Finally, there is the obvious lower-level optimization: both Matt and Benjamin's solutions are in
+Python, this solution is in Rust.
 
-Annecdotal timings on laptops have been the standard thusfar. For me this runs in under two and a
-half minutes. That's running as `cargo run --release word_alpha.txt`
-
-There may still be optimization to be had here. For example, if `a b c d e` and `a c d e f` are both
-correct results, the determination that `a b` is disjoint from `c` for the first result will not be
-reused to determine `a` is disjoint from `c` when materializing the second result. A more
-complicated data structure while iterating might make this possible, trading a bit of memory for
-some comparisons, though there may be some difficulty avoiding allocations to manage this more
-complex structure.
+Annecdotal timings on laptops have been the standard thusfar. For me this runs in under forty
+seconds.  That's running as `cargo run --release word_alpha.txt`
--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,24 @@ use std::env::args;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::iter::Iterator;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::thread::scope;
 
-/// Iterates a B-Tree with `u32` keys and yields the keys which are greater than or equal to the
-/// given `start`, and which have no bits in common with the given `start`.
-struct Iter<'a> {
-    word_list: &'a BTreeMap<u32, Vec<String>>,
-    filter: u32,
-    last_yielded: u32,
+struct Node {
+    key: u32,
+    children: Vec<Node>,
 }
 
-impl<'a> Iter<'a> {
-    fn new(word_list: &'a BTreeMap<u32, Vec<String>>, start: u32) -> Iter<'a> {
-        Iter {
-            word_list,
-            filter: start,
-            last_yielded: start,
-        }
-    }
-}
-
-impl Iterator for Iter<'_> {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<u32> {
-        let mut next = next_with_none_in_common(self.last_yielded, self.filter);
-
-        while let Some((&k, _)) = self.word_list.range(next..).next() {
-            if (k & self.filter) == 0 {
-                self.last_yielded = k;
-                return Some(k);
+impl Node {
+    fn insert(&mut self, key: u32) {
+        for child in self.children.iter_mut() {
+            if child.key & key == 0 {
+                child.insert(key);
             }
-
-            next = next_with_none_in_common(k, self.filter);
         }
-
-        None
+        self.children.push(Node {
+            key,
+            children: Vec::new(),
+        });
     }
-}
-
-/// Given a u32 value, finds the next largest value which contains none of the bits set in the given
-/// `mask`.
-#[inline]
-const fn next_with_none_in_common(val: u32, mask: u32) -> u32 {
-    let common = val & mask;
-    let eligible_zeroes = !val & !((common + 1).next_power_of_two() - 1);
-    let eligible_mask = eligible_zeroes ^ (eligible_zeroes - 1);
-    (val | eligible_mask) & !(eligible_mask >> 1)
 }
 
 fn main() {
@@ -75,70 +45,48 @@ fn main() {
         word_list.entry(key).or_insert_with(Vec::new).push(line);
     }
 
-    let found = AtomicUsize::new(0);
-    let found_anagrams = AtomicUsize::new(0);
+    let mut root = Node {
+        key: 0,
+        children: Vec::with_capacity(word_list.len()),
+    };
 
-    scope(|s| {
-        let cpus = num_cpus::get();
-        let word_list = &word_list;
-        let found = &found;
-        let found_anagrams = &found_anagrams;
-        for cpu in 0..cpus {
-            s.spawn(move || {
-                for result in word_list
-                    .keys()
-                    .skip(cpu)
-                    .step_by(cpus)
-                    .copied()
-                    .flat_map(|a| Iter::new(word_list, a).map(move |b| (vec![a, b], a | b)))
-                    .flat_map(|(accum, mask)| {
-                        Iter::new(word_list, mask).map(move |new| {
-                            let mut accum = accum.clone();
-                            accum.push(new);
-                            (accum, mask | new)
-                        })
-                    })
-                    .flat_map(|(accum, mask)| {
-                        Iter::new(word_list, mask).map(move |new| {
-                            let mut accum = accum.clone();
-                            accum.push(new);
-                            (accum, mask | new)
-                        })
-                    })
-                    .flat_map(|(accum, mask)| {
-                        Iter::new(word_list, mask).map(move |new| {
-                            let mut accum = accum.clone();
-                            accum.push(new);
-                            accum
-                        })
-                    })
-                {
-                    let mut total = 1;
-                    let mut got = Vec::with_capacity(result.len());
-                    for (word, count) in result.into_iter().map(|x| {
-                        let words = word_list.get(&x).unwrap();
-                        (words[0].clone(), words.len())
-                    }) {
-                        total *= count;
-                        got.push(word);
-                    }
+    for key in word_list.keys().copied() {
+        root.insert(key);
+    }
 
-                    let got = got.join(", ");
+    let mut found = 0;
+    let mut found_anagrams = 0;
 
-                    if total == 1 {
-                        println!("{got}");
-                    } else {
-                        println!("{got} (plus {} others with anagrams)", total - 1);
-                    }
-                    found.fetch_add(1, Ordering::Relaxed);
-                    found_anagrams.fetch_add(total, Ordering::Relaxed);
-                }
-            });
+    for item in root
+        .children
+        .into_iter()
+        .flat_map(|x| x.children.into_iter().map(move |y| (y, x.key)))
+        .flat_map(|(x, a)| x.children.into_iter().map(move |y| (y, a, x.key)))
+        .flat_map(|(x, a, b)| x.children.into_iter().map(move |y| (y, a, b, x.key)))
+        .flat_map(|(x, a, b, c)| x.children.into_iter().map(move |y| [a, b, c, x.key, y.key]))
+    {
+        let mut anagrams = 1;
+        let got = item
+            .iter()
+            .map(|f| {
+                let list = word_list.get(f).unwrap();
+                anagrams *= list.len();
+                list[0].clone()
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        if anagrams == 1 {
+            println!("{got}")
+        } else if anagrams == 2 {
+            println!("{got} (plus 1 other with anagrams)")
+        } else {
+            println!("{got} (plus {} others with anagrams)", anagrams - 1)
         }
-    });
+        found += 1;
+        found_anagrams += anagrams;
+    }
 
-    let found = found.load(Ordering::Relaxed);
-    let found_anagrams = found_anagrams.load(Ordering::Relaxed);
     println!();
     println!("Total: {found} ({found_anagrams} with anagrams)");
 }
